d5f472ba7f3a7581deb044e1ec08fefe6dcbb41a
黒田健太 <96ken@kurodakentanoMacBook-Air.local>
2020-02-16 00:43:54 +0900

    [add]Bookers2いいねｺﾒﾝﾄ

---
+++ Bookers2-add/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e0740e5a1eeef5ef684afa38237a0eb7401dc221025c11abfa6e06fda8ebcf88f983939669f873594ddbaee4de603c062c782e2d0c1be42ff12e9dae9fabe9f7'
