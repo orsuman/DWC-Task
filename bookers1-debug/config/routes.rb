5894b210a44616d612cd3841bf675d4120eaaac1
黒田健太 <96ken@kurodakentanoMacBook-Air.local>
2020-03-07 14:17:20 +0900

    [Fix]課題１update修正

--- bookers1-debug/config/routes.rb
+++ bookers1-debug/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :books
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
   root to: 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
